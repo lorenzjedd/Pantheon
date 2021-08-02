1d31c8989c52ef81434afcd444b66bb77ec0a648
SystemNULL <74456205+SystemNULL@users.noreply.github.com>
2021-08-02 23:44:38 +0800

    Add files via upload
    Profile

---
+++ Profile.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap');
+        
+        * {box-sizing: border-box; margin: 0; padding: 0; font-family: "Poppins";}
+        body {background-color: honeydew;}
+
+        header {margin: 7em 0 5em;}
+        .name {letter-spacing: 2px; text-align: center; font-size: 30px;}
+
+        .hero {margin: 0 0 5em;}
+        .hero p {letter-spacing: 2px; line-height: 85px; text-align: center; font-size: 80px; font-weight: 600;}
+
+        .biography {margin: 0 25em;}
+        .biography p {letter-spacing: 5px; text-align: center; font-size: 25px; font-weight: 300;}
+    </style>
+  </head>
+  <body>
+    <header>
+      <p class="name">Lorenz Jedd</p>
+    </header>
+
+    <section>
+      <div class="hero">
+        <p>Learner</p>
+        <p>Sophomore</p>
+        <p>ComScie Student</p>
+      </div>
+      <div class="biography">
+        <p>
+          I study at the University of Makati and Programming is what I really love. I have some knowledge in C++, VB.Net, Java, HTML, CSS, BootStrap, JavaScript and PHP. As time goes by, I've already done some projects that were tasked by my previous professors in the previous year levels. I also tried joining some programming-related competitions to gain more experience in coding.
+        </p>
+      </div>
+    </section>
+  </body>
+</html>
